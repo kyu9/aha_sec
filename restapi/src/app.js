5220f8b354824f811313f409b7c967abcdac898a
kyu9 <kyudo97@naver.com>
2020-07-23 16:53:42 +0900

    last commit

--- restapi/src/app.js
+++ restapi/src/app.js
@@ -3,20 +3,26 @@ require('dotenv').config()
 import createError from 'http-errors'
 import express from 'express'
 import cookieParser from 'cookie-parser'
-import logger from 'morgan'
 import response from './utils/response'
+import moment from 'moment'
 import v1Route from './routes/v1'
 import jwtMiddleware from './middlewares/jwt.middleware'
+import morgan from 'morgan'
+import {logger, stream} from './configs/winston'
 
 const app = express();
+const sentry = require('@sentry/node')
+sentry.init({dsn: process.env.SENTRY_DSN})
 
-app.use(logger('dev'));
+app.use(morgan('combined', {stream}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(jwtMiddleware)
 
 app.use('/v1', v1Route)
+
+app.use(sentry.Handlers.errorHandler())
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,9 +37,28 @@ app.use((err, req, res, next) => {
     apiError = createError(err)
   }
 
-  // set locals, only providing error in development
-  res.locals.message = apiError.message
-  res.locals.error = process.env.NODE_ENV === 'development' ? apiError : {}
+  if(process.env.NODE_ENV === 'test'){
+    const errObj = {
+      req:{
+        headers: req.headers,
+        query: req.query,
+        body: req.body,
+        route: req.route
+      },
+      error: {
+        message: apiError.message,
+        stack: apiError.stack,
+        status: apiError.status
+      },
+      user: req.user
+    }
+    logger.error(`${moment().format('YYYY-MM-DD HH:mm:ss')}`, errObj)
+  }else{
+    res.locals.message = apiError.message
+    res.locals.error = apiError
+  }
+
+
 
   // render the error page
   return response(res, {
