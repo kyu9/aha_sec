7a4ecbbe24a9effba634ed6bce4f71d8df1cad10
kyu9 <kyudo97@naver.com>
2020-07-24 15:12:26 +0900

    a

--- restapi/src/app.js
+++ restapi/src/app.js
@@ -24,6 +24,7 @@ app.use('/v1', v1Route)
 
 app.use(sentry.Handlers.errorHandler())
 
+/*
 app.get('/',function(req, res, next){
   res.send('Run!')
 })
@@ -31,6 +32,7 @@ app.get('/',function(req, res, next){
 app.get('/users', function(req, res, next){
   res.send('user page')
 })
+*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
